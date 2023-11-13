6099ede541bae9b375be868de238cb6acc38157e
kevin-coaquira <kcoaquirachoquecallata@cifpfbmoll.eu>
2023-11-13 20:27:56 +0100

    port

--- config/database.php
+++ config/database.php
@@ -3,6 +3,7 @@
 class Database {
     // Especifique sus propias credenciales de base de datos
     private $host = "adminer.randion.es";
+    private $port = "5432";
     private $db_name = "kcoaquira_login_system";
     private $username = "kcoaquirachoquecallata";
     private $password = "Secretos.2023";
@@ -13,7 +14,7 @@ class Database {
         $this->conn = null;
         try {
             // Use el controlador pgsql para PostgreSQL
-            $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("pgsql:host=" . $this->host . ";port=" . $this->port .";dbname=" . $this->db_name, $this->username, $this->password);
             // Establecer el modo de error PDO a excepción
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $exception) {
